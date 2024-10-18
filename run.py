29ca9b16cd3885520ee16338dc09614797d6fe92
a6000 <nhatho1999@github.com>
2024-10-18 16:47:12 +0700

    deploy

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 from utils.prod_config import SERVER_GRADIO_PORT, SERVER_STREAMLIT_PORT
-
+os.system(f"apt-get install psmisc")
 os.system(f"fuser -k {SERVER_GRADIO_PORT}/tcp")
 os.system(f"fuser -k {SERVER_STREAMLIT_PORT}/tcp")
 os.system(f"python app.py & streamlit run st_monitor.py --client.showSidebarNavigation=False --server.fileWatcherType none --browser.gatherUsageStats=False --server.port {SERVER_STREAMLIT_PORT}")
